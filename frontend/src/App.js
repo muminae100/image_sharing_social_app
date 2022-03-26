d96a49ea54971559b5e33c7ba8393727a79f7354
foreverkenyan <smuminaetx100@gmail.com>
2022-03-26 17:32:07 +0300

    Install all other project dependencies

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,10 @@
-import './App.css';
+import React from 'react';
 
 function App() {
   return (
-    <div className="App">
+    <h1 className="text-3xl font-bold underline">
       Hello world!
-    </div>
+    </h1>
   );
 }
 
