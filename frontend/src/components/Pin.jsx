b5274544deae8a6f4b3c8fdc8e516135dc8cea9c
foreverkenyan <smuminaetx100@gmail.com>
2022-03-28 22:55:43 +0300

    Fix pin.jsx hover effect

--- frontend/src/components/Pin.jsx
+++ frontend/src/components/Pin.jsx
@@ -8,7 +8,7 @@ import { BsFillArrowUpRightCircleFill } from 'react-icons/bs';
 import { client, urlFor } from '../client';
 
 const Pin = ({ pin }) => {
-  const [postHovered, setPostHovered] = useState(true);
+  const [postHovered, setPostHovered] = useState(false);
   const [savingPost, setSavingPost] = useState(false);
 
   const navigate = useNavigate();
@@ -55,8 +55,8 @@ const Pin = ({ pin }) => {
   return (
     <div className="m-2">
       <div
-        // onMouseEnter={() => setPostHovered(true)}
-        // onMouseLeave={() => setPostHovered(false)}
+        onMouseEnter={() => setPostHovered(true)}
+        onMouseLeave={() => setPostHovered(false)}
         onClick={() => navigate(`/pin-detail/${_id}`)}
         className="relative cursor-zoom-in w-auto hover:shadow-lg rounded-lg overflow-hidden transition-all duration-500 ease-in-out"
       >
